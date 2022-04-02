1b5dc78670acf229fa1d57cab951eecd48ce593f
Reonu <danileon95@gmail.com>
2022-04-02 18:30:50 +0300

    level changes

--- levels/hmc/texscroll.inc.h
+++ levels/hmc/texscroll.inc.h
@@ -1,4 +1,5 @@
-extern void scroll_hmc_dl_Plane_001_mesh_layer_5_vtx_0();
+extern void scroll_hmc_dl_Plane_001_mesh_layer_1_vtx_0();
+extern void scroll_sts_mat_hmc_dl_Zel_water_1_layer1();
 extern void scroll_sts_mat_hmc_dl_waterOpaque_layer1();
 extern void scroll_sts_mat_hmc_dl_waterTransparent_layer5();
 extern void scroll_hmc();
