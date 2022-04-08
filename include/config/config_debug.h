0308d960647b6a11629640b658b43fbff73bfb37
Reonu <danileon95@gmail.com>
2022-04-08 07:21:33 +0300

    fix repo

--- include/config/config_debug.h
+++ include/config/config_debug.h
@@ -15,7 +15,7 @@
 // This allows you to quickly test the level you're working on.
 // If you want the game to boot normally, just comment out the define again.
 
-#define TEST_LEVEL LEVEL_WF
+//#define TEST_LEVEL LEVEL_WF
 
 // Enable debug level select. Hold L while the game boots to turn it on.
 #define DEBUG_LEVEL_SELECT
